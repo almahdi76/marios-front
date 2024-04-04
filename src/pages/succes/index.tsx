4449dec56d07c7e7b223acd70b7e67e3d2bb11d5
almahdi76 <almahdi.abusef@yahoo.com>
2024-04-04 16:53:11 +0200

    page secces

--- src/pages/succes/index.tsx
+++ src/pages/succes/index.tsx
@@ -1,10 +1,22 @@
+import { Card, CardContent } from '@mui/material'
 import React from 'react'
 
 const Succes = () => {
   return (
-    <div>
-      <h1>falicitation</h1>
-    </div>
+    <div style={{margin:'auto',width:"80%",marginTop:"30px",paddingLeft:"auto"}}>
+  
+    
+    <Card sx={{ minWidth: 275,maxWidth:"70%" }}>
+    <CardContent style={{background:'#3b438b'}}>
+   
+    <h2 style={{color:"#fbc02c"}}>Felicitation  </h2>
+   
+     
+   
+    </CardContent>
+    </Card>
+    {/* +++++++++++++++++++++++++++++++++++ */}
+  </div>
   )
 }
 
